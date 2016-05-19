1b16222f1aa3b61ff5b3ef02c2690bfeb1c1c657
Audio88 <willgriffith3@hotmail.com>
2016-05-19 12:36:50 -0700

    Update exercise2.js

--- exercise2.js
+++ exercise2.js
@@ -4,8 +4,10 @@ function countDown(arr, count) {
 	if (count === undefined) {
         count = 0;
     }
-   	console.log(arr[count]);
     if (count == arr.length) return "done";
-    else return countDown(arr, count += 1);
+    else {
+    console.log(arr[count]);
+    return countDown(arr, count += 1);
+    }
 }
 countDown(tools);
